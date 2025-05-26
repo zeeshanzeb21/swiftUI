43b1a3b6c994a2b0176c49d755b378447f92d03c
zeeshanzeb21 <zeeshan.zeb@invicttus.com>
2025-05-26 10:04:34 +0500

    added a list in web screen

---
+++ swiftUI/CustomStyles/ListButtons.swift
@@ -0,0 +1,18 @@
+//
+//  ListButtons.swift
+//  swiftUI
+//
+//  Created by Invicttus on 23/05/2025.
+//
+
+import SwiftUI
+
+struct ListButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListButtons()
+}
---
+++ swiftUI/WebScreen/WebScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WebScreen.swift
+//  swiftUI
+//
+//  Created by Invicttus on 23/05/2025.
+//
+
+import SwiftUI
+
+struct WebScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebScreen()
+}
