0596adf7dd03db9dcd0a2bb9e259dde209dd4a14
zeeshanzeb21 <zeeshan.zeb@invicttus.com>
2025-05-27 09:03:42 +0500

    web page 80% done

--- swiftUI/CustomStyles/CustomTextFieldStyle.swift
+++
@@ -1,7 +0,0 @@
-//
-//  CustomTextFieldStyle.swift
-//  swiftUI
-//
-//  Created by Invicttus on 21/05/2025.
-//
-
---
+++ swiftUI/WebScreen/WebViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewModel.swift
+//  swiftUI
+//
+//  Created by Invicttus on 26/05/2025.
+//
+
+import SwiftUI
+
+struct WebViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebViewModel()
+}
