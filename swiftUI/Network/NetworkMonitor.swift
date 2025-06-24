ef34b3241670760b21a8ddbb83f2b111ac109fd0
zeeshanzeb21 <zeeshan.zeb@invicttus.com>
2025-06-24 12:30:58 +0500

    worked on setting internet connectivity checks on overall screens 100% done

---
+++ swiftUI/Network/NetworkMonitor.swift
@@ -0,0 +1,22 @@
+import Network
+import Foundation
+
+class NetworkMonitor: ObservableObject {
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitor")
+
+    @Published var isConnected: Bool = true
+
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+            }
+        }
+        monitor.start(queue: queue)
+    }
+
+    deinit {
+        monitor.cancel()
+    }
+}
